7a399866e4257322279ce56e01964c156913e6d3
IrynaDenysovich <106885843+IrynaDenysovich@users.noreply.github.com>
2023-01-25 15:16:41 +0200

    fix request axios

--- src/components/api.js
+++ src/components/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-//axios.defaults.baseURL = 'https://pixabay.com/';
+axios.defaults.baseURL = 'https://pixabay.com';
 const commonKey = '31801640-92314b1461717efb7747c4e31';
 
 export const fetchImages = async (query, page, per_page) => {
-  const response = await axios.get('https://pixabay.com/api', {
+  const response = await axios.get('/api/', {
     params: {
       q: query,
       key: commonKey,
